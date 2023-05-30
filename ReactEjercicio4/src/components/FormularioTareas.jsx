bcd96efa1adace2045eaa512f89c15976472ac6d
cristianq3 <cristianq3@gmail.com>
2023-05-30 12:14:31 -0300

    Se agregan eventos Submit y onchange

--- ReactEjercicio4/src/components/FormularioTareas.jsx
+++ ReactEjercicio4/src/components/FormularioTareas.jsx
@@ -3,21 +3,33 @@ import Form from "react-bootstrap/Form";
 import ListaTareas from "./ListaTareas";
 import { useState } from "react";
 
-function Input() {
+function FormularioTarea() {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  };
+
   return (
-    <section> 
-      <h1 className="display-3">Lista de Tareas</h1>
-    <Form>
-      <Form.Group className="mb-3 d-flex" controlId="tarea">
-        <Form.Control type="text" placeholder="Ingrese la tarea" />
-        <Button variant="warning" className="ms-2" type="submit">
-        <i className="bi bi-plus"></i>
-        </Button>
-      </Form.Group>
-    </Form>
-    <ListaTareas></ListaTareas>
+    <section>
+      <h1 className="display-3 mb-3">Lista de Tareas</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex" controlId="tarea">
+          <Form.Control
+            type="text"
+            placeholder="Ingrese la tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button variant="warning" className="ms-2" type="submit">
+            <i className="bi bi-plus"></i>
+          </Button>
+        </Form.Group>
+      </Form>
+      <ListaTareas></ListaTareas>
     </section>
   );
 }
 
-export default Input;
+export default FormularioTarea;
